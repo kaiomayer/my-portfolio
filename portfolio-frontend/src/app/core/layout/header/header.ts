674e0de237e5f852de2116800d782e5cd6d8ef1d
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-13 18:05:48 -0300

    feat: implemented header core

--- portfolio-frontend/src/app/core/layout/header/header.ts
+++ portfolio-frontend/src/app/core/layout/header/header.ts
@@ -1,11 +1,37 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+import {NavItem} from '../../../shared/models/navItem.model';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
 export class Header {
+
+  public navItems: NavItem[] = [
+    {
+      "text": "Sobre mim",
+      "url": "/about-me"
+    },
+    {
+      "text": "Projetos",
+      "url": "/projects"
+    },
+    {
+      "text": "Experiência",
+      "url": "/experience"
+    },
+    {
+      "text": "Contato",
+      "url": "/contact"
+    }
+  ];
+
 
 }
