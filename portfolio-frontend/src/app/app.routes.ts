10b0622c4f42cf439f72c7c5607f986b704e38f5
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-13 17:33:21 -0300

    chore: added basic routes to the application

--- portfolio-frontend/src/app/app.routes.ts
+++ portfolio-frontend/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
+import {Projects} from './domain/projects/projects';
+import {Experience} from './domain/experience/experience';
+import {AboutMe} from './domain/about-me/about-me';
+import {Contact} from './domain/contact/contact';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/about-me',
+    pathMatch: 'full',
+  },
+  {
+    path: 'about-me',
+    component: AboutMe
+  },
+  {
+    path: 'projects',
+    component: Projects
+  },
+  {
+    path: 'experience',
+    component: Experience
+  },
+  {
+    path: 'contact',
+    component: Contact
+  }
+];
