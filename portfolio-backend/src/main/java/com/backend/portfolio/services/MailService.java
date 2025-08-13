9ea9a2c32c22a44d21d43d5ed34f198f70936205
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-13 12:51:00 -0300

    hotfix: fixed the springboot live reload issues by adding a goddamn dns server reference

---
+++ portfolio-backend/src/main/java/com/backend/portfolio/services/MailService.java
@@ -0,0 +1,11 @@
+package com.backend.portfolio.services;
+
+import org.springframework.stereotype.Service;
+
+import lombok.NoArgsConstructor;
+
+@Service
+@NoArgsConstructor
+public class MailService {
+
+}
