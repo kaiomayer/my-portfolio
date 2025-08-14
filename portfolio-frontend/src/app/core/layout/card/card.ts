95b26ca1f12c39fc2abd4a06361cf712e13d28b5
MateusRodrigu <164748740+MateusRodrigu@users.noreply.github.com>
2025-08-13 21:02:58 -0300

    teste de card
    Primeiro teste de card para os projetos apeans teset

---
+++ portfolio-frontend/src/app/core/layout/card/card.ts
@@ -0,0 +1,21 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+
+/**
+ * @title Card overview
+ */
+@Component({
+  selector: 'app-card',
+  templateUrl: 'card.html',
+  styleUrl: 'card.scss',
+  imports: [MatCardModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true
+})
+export class Card {}
+
+
+/**  Copyright 2025 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
--- portfolio-frontend/src/app/domain/projects/projects.ts
+++ portfolio-frontend/src/app/domain/projects/projects.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Card } from '../../core/layout/card/card';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [Card],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
