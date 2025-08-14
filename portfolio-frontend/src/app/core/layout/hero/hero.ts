943320e00ea9821cb9f9a81ad3398a35531948ae
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-14 01:31:51 -0300

    feat: hero component

---
+++ portfolio-frontend/src/app/core/layout/hero/hero.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import {StandardLightParagraph} from '../../../shared/components/standard-light-paragraph/standard-light-paragraph';
+
+@Component({
+  selector: 'app-hero',
+  imports: [
+    StandardLightParagraph
+  ],
+  templateUrl: './hero.html',
+  styleUrl: './hero.scss'
+})
+export class Hero {
+  @Input() title!: string;
+  @Input() description!: string;
+}
