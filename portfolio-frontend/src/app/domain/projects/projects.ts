b057b1f9521793dcad63a0028d8fc702a90c134f
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-14 19:51:22 -0300

    style: add animation in project's title

--- portfolio-frontend/src/app/domain/projects/projects.ts
+++ portfolio-frontend/src/app/domain/projects/projects.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { Card } from '../../core/layout/card/card';
+import {Hero} from '../../core/layout/hero/hero';
+
 
 @Component({
   selector: 'app-projects',
-  imports: [Card],
+  imports: [Card, Hero],
   templateUrl: './projects.html',
   styleUrl: './projects.scss'
 })
+
 export class Projects {
 
 }
