15236ce8752be0aacecafbee3b0345075b7f4126
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-14 14:49:52 -0300

    feat: created mail sender service

--- portfolio-backend/src/main/java/com/backend/portfolio/services/MailService.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/services/MailService.java
@@ -1,5 +1,9 @@
 package com.backend.portfolio.services;
 
+import com.backend.portfolio.dtos.MailDTO;
+import com.backend.portfolio.utils.MailUtils;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import lombok.NoArgsConstructor;
@@ -7,5 +11,20 @@ import lombok.NoArgsConstructor;
 @Service
 @NoArgsConstructor
 public class MailService {
+    private JavaMailSender mailSender;
+    private final String userEmail = System.getenv("EMAIL");
 
+    public MailService(JavaMailSender mailSender){
+        this.mailSender = mailSender;
+    }
+
+    public void sendSimpleMail(MailDTO mail){
+        SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom(userEmail);
+        email.setTo(userEmail);
+        email.setSubject(mail.subject());
+        String body = MailUtils.formatBody(mail);
+        email.setText(body);
+        this.mailSender.send(email);
+    }
 }
