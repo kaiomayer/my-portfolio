cb36c436f5869fed4eb61854acc237564c83e85e
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-17 16:48:56 -0300

    feat: added a dto for login

---
+++ portfolio-backend/src/main/java/com/backend/portfolio/Dtos/LoginDTO.java
@@ -0,0 +1,10 @@
+package com.backend.portfolio.Dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record LoginDTO(
+        @NotBlank @Size(min = 5, max = 64) String username,
+        @NotBlank String password
+) {
+}
