2c6192d2759e64dc57dcf543e35d16b89055a471
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 09:01:30 -0300

    refactor: record to class

--- portfolio-backend/src/main/java/com/backend/portfolio/Controllers/AuthController.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Controllers/AuthController.java
@@ -2,9 +2,13 @@ package com.backend.portfolio.Controllers;
 
 import com.backend.portfolio.Dtos.LoginDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.token.TokenService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+    @Autowired
+    private TokenService tokenService;
 
     @PostMapping(name = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> login(@Valid @RequestBody LoginDTO loginData) {
         String token = "";
+        try {
+            UsernamePasswordAuthenticationToken userData = new UsernamePasswordAuthenticationToken()
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha inválidos");
+
+        }
         return ResponseEntity.ok().body(token);
     }
 
     @PostMapping(name = "logout", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<HttpStatus> logout(){
+    public ResponseEntity<HttpStatus> logout() {
         return ResponseEntity.noContent().build();
     }
 }
--- portfolio-backend/src/main/java/com/backend/portfolio/Dtos/LoginDTO.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Dtos/LoginDTO.java
@@ -2,9 +2,17 @@ package com.backend.portfolio.Dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
-public record LoginDTO(
-        @NotBlank @Size(min = 5, max = 64) String username,
-        @NotBlank String password
-) {
+
+public class LoginDTO {
+    @NotBlank
+    @Size(min = 5, max = 64)
+    private String username;
+
+    @NotBlank
+    private String password; 
 }
+
