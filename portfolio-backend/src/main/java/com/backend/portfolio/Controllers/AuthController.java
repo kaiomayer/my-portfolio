a2774bbaf38401dd02ae92d179e2a793c3a51b34
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 10:34:53 -0300

    feat: generate token and set it into header

--- portfolio-backend/src/main/java/com/backend/portfolio/Controllers/AuthController.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Controllers/AuthController.java
@@ -1,12 +1,12 @@
 package com.backend.portfolio.Controllers;
 
 import com.backend.portfolio.Dtos.LoginDTO;
+import com.backend.portfolio.Models.User;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.apache.tomcat.util.http.SameSiteCookies;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -27,18 +27,30 @@ public class AuthController {
     private AuthenticationManager authenticationManager;
 
     @PostMapping(name = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<String> login(@Valid @RequestBody LoginDTO loginData, HttpServletResponse response) {
+    public ResponseEntity<?> login(@Valid @RequestBody LoginDTO loginData, HttpServletResponse response) {
         String token = "";
         try {
             UsernamePasswordAuthenticationToken userData = new UsernamePasswordAuthenticationToken(loginData.getUsername(),
                     loginData.getPassword());
             Authentication auth = authenticationManager.authenticate(userData);
 
+            token = tokenService.generateToken((User) auth.getPrincipal());
+
+            ResponseCookie cookie = ResponseCookie.from("token", token)
+                    .httpOnly(true)
+                    .maxAge(24 * 60 * 60)
+                    .secure(false)
+                    .sameSite(SameSiteCookies.STRICT.toString())
+                    .path("/")
+                    .build();
+
+            response.addHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+            return ResponseEntity.ok(auth);
+
         } catch (AuthenticationException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário ou senha inválidos");
 
         }
-        return ResponseEntity.ok().body(token);
     }
 
     @PostMapping(name = "logout", consumes = MediaType.APPLICATION_JSON_VALUE)
