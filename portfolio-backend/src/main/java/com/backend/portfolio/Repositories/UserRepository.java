3c17c8b2ab81f902eea725d14ba253d836cdb592
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 08:50:10 -0300

    feat: findByUsername in UserRepository

--- portfolio-backend/src/main/java/com/backend/portfolio/Repositories/UserRepository.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.backend.portfolio.Repositories;
 
 import com.backend.portfolio.Models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, String> {
+    UserDetails findByUsername(String username);
 }
