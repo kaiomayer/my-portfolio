c3a441a975ce6a347c2deff39946675041c4adf7
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 08:42:07 -0300

    feat: get username and password

--- portfolio-backend/src/main/java/com/backend/portfolio/Models/User.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Models/User.java
@@ -63,4 +63,14 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return UserDetails.super.isEnabled();
     }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
 }
