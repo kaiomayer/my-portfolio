af167edb36ad023a19f731623254b5ae5e472e3e
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 22:12:14 -0300

    fix: fixing request matchers

--- portfolio-backend/src/main/java/com/backend/portfolio/Configs/SecurityConfig.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Configs/SecurityConfig.java
@@ -29,8 +29,8 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/register").permitAll()
                 )
                 .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
