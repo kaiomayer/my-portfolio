9ed3faa76e457395c62a9616df78505dcb97d4a8
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-18 21:57:11 -0300

    feat: security filter filter internal

---
+++ portfolio-backend/src/main/java/com/backend/portfolio/Security/SecurityFilter.java
@@ -0,0 +1,49 @@
+package com.myapp.estoque.security;
+
+import com.backend.portfolio.Repositories.UserRepository;
+import com.backend.portfolio.Services.TokenJWTService;
+import com.myapp.estoque.repository.UsuarioRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenJWTService tokenJWTService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = recoverToken(request);
+
+        if (token != null) {
+            String login = tokenJWTService.validateToken(token);
+            UserDetails userDetails = userRepository.findByUsername(login);
+
+            System.out.println(userDetails);
+
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+    
+}
