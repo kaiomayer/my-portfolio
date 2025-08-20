d86abcfc044ca058c3791bc8484c3aae59cfe3ea
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-20 11:59:34 -0300

    feat: a error message class to custom responses

---
+++ portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/ErrorMessage.java
@@ -0,0 +1,8 @@
+package com.backend.portfolio.Exceptions;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorMessage {
+    private String message;
+    private HttpStatus status;
+}
--- portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/GlobalExceptionHandler.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/GlobalExceptionHandler.java
@@ -1,9 +1,13 @@
 package com.backend.portfolio.Exceptions;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<?> userNotFoundException(UserNotFoundException exception) {
+        
+    }
 }
