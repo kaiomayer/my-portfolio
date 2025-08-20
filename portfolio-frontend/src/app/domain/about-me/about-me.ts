302557a1bceea4dd7f5e960496ec1122fd4464b6
Kaio Mayer <99258505+kaiomayer@users.noreply.github.com>
2025-08-20 17:12:40 -0300

    feat: using button component in about-me

--- portfolio-frontend/src/app/domain/about-me/about-me.ts
+++ portfolio-frontend/src/app/domain/about-me/about-me.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import 'animate.css';
+import { ButtonComponent } from '../../core/layout/button/button.component';
+import { Router } from '@angular/router';
 
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss'
 })
 export class AboutMe {
+  constructor(private router: Router) {}
 
+  handleButtonClick(): void {
+    this.router.navigate(['/projects']);  
+  }
 }
