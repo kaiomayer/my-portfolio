a6b3d41b3a5dfdf823a56bbafd066fbf9c00b450
Arthur Bacelete <164350625+bacelete@users.noreply.github.com>
2025-08-20 12:09:19 -0300

    feat: returning a custom error message exception

--- portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/ErrorMessage.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/ErrorMessage.java
@@ -1,7 +1,13 @@
 package com.backend.portfolio.Exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
+@AllArgsConstructor
+@Getter
+@Setter
 public class ErrorMessage {
     private String message;
     private HttpStatus status;
--- portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/GlobalExceptionHandler.java
+++ portfolio-backend/src/main/java/com/backend/portfolio/Exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.backend.portfolio.Exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<?> userNotFoundException(UserNotFoundException exception) {
-        
+        ErrorMessage error = new ErrorMessage(exception.getMessage(), HttpStatus.NOT_FOUND);
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
     }
 }
