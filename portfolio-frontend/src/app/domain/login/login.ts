1de78d5b0acf3e709374bd6743b4b9a4320c5108
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-20 15:44:52 -0300

    feat: finsihed login screen

--- portfolio-frontend/src/app/domain/login/login.ts
+++ portfolio-frontend/src/app/domain/login/login.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import {GenericInput} from '../../shared/components/generic-input/generic-input';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {LoginPayload} from '../../shared/models/login.model';
+import {AuthService} from '../../core/services/AuthService/auth-service';
+import {Router} from '@angular/router';
+import {Hero} from '../../core/layout/hero/hero';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   imports: [
     GenericInput,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    Hero,
+    MatButton
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
@@ -15,7 +21,7 @@ import {LoginPayload} from '../../shared/models/login.model';
 export class Login {
   public loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private authService : AuthService, private router : Router) {
     this.loginForm = this.formBuilder.group({
       username: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
@@ -26,7 +32,32 @@ export class Login {
     return this.loginForm.value as LoginPayload;
   }
 
-  onSubmit() {
+  public getControl(control : string) {
+    return this.loginForm.get(control) as FormControl;
+  }
 
+  onSubmit() {
+    this.authService.login(this.preparePayload()).subscribe(
+      {
+        next: () => {
+          console.table(this.preparePayload());
+        },
+        error: err => {
+          console.error(`There was an error trying to log the user in: ${err.message}`);
+          this.loginForm.reset();
+        },
+        complete: () => {
+          this.router.navigate(['edit']).then(navigationSuccessful => {
+            if (navigationSuccessful) {
+              console.log('Navigation to edit page was successful!');
+            } else {
+              console.log('Navigation was cancelled by a guard.');
+            }
+          }).catch(error => {
+              console.error('Navigation failed:', error);
+            });
+        }
+      }
+    );
   }
 }
