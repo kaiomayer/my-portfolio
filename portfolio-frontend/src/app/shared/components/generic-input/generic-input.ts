b9ab9c2d881735292bb95ed2d412b54ebb3eacb7
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-20 15:44:31 -0300

    feat: made certain fields optional

--- portfolio-frontend/src/app/shared/components/generic-input/generic-input.ts
+++ portfolio-frontend/src/app/shared/components/generic-input/generic-input.ts
@@ -11,10 +11,10 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 })
 export class GenericInput {
   @Input() inputType: string = 'text';
-  @Input() placeholder: string = '';
+  @Input() placeholder?: string = '';
   @Input() label: string = '';
   @Input() disabled: boolean = false;
-  @Input() id: string = `id-${crypto.randomUUID()}`;
+  @Input() id?: string = `id-${crypto.randomUUID()}`;
   @Input() required: boolean = false;
   @Input() control!: FormControl;
 }
