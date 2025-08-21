28493c286c7ed63c0464f98556ee0b3a145239ac
Gabriel Nogueira Vieira Resende <gabrielnvresende@gmail.com>
2025-08-21 17:20:27 -0300

    feat: added cv download in the about me page

--- portfolio-frontend/src/app/domain/about-me/about-me.ts
+++ portfolio-frontend/src/app/domain/about-me/about-me.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import 'animate.css';
 import { ButtonComponent } from '../../core/layout/button/button.component';
 import { Router } from '@angular/router';
+import {MatButton} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-about-me',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, MatButton],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss'
 })
@@ -14,6 +15,6 @@ export class AboutMe {
   constructor(private router: Router) {}
 
   handleButtonClick(): void {
-    this.router.navigate(['/projects']);  
+    this.router.navigate(['/projects']);
   }
 }
